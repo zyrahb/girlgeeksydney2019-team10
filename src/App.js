fa0998efe5ae2f4d6d90d1f4f820e957e5b7884d
Josephine Chong <josephine.yt.chong@gmail.com>
2019-07-13 15:27:55 +1000

    added images

--- src/App.js
+++ src/App.js
@@ -1,29 +1,66 @@
 import React, { Component } from 'react';
 import { Map } from './Map';
+import './app.css';
+
+const infoboxTemplate = (options) => {
+  return (
+    <div class="customInfobox">
+      <div class="title">{options.title}</div>
+      {options.descriptions && options.descriptions.map(item => {
+        return <div>{item}</div>
+      })}
+      {options.description}
+      {options.image && <img src={options.image} />}
+    </div>);
+}
 
 const lemonPushPins = [
   {
     location: [-33.8844642, 151.2078435],
     addHandler: "mouseover",
-    infoboxOption: { title: "Alan's Lovely lemons", description: 'Contact Alan (+61 465 876 2345) for some lemons' },
+    infoboxOption: {
+      htmlContent: infoboxTemplate({
+        title: "Alan's Lovely lemons", 
+        descriptions: ['Contact Alan', '(+61 465 876 2345)','for some lemons'],
+        image: 'lemons.jpg'
+      })
+     },
     pushPinOption: { title: "Alan's Lovely lemons", description: 'Pushpin', icon: "icon.svg" },
   },
   {
     location: [-33.8584776, 151.208703],
     addHandler: "mouseover",
-    infoboxOption: { title: "Zoe's zesty lemons", description: 'Contact Zoe (+61 465 876 2346) for some lemons' },
+    infoboxOption: { 
+      htmlContent: infoboxTemplate({ 
+        title: "Zoe's zesty lemons", 
+        description: ['Contact Zoe', '(+61 465 876 2346)', 'for some lemons'], 
+        image: 'lemons.jpg'
+        })
+       },
     pushPinOption: { title: "Zoe's zesty lemons", description: 'Pushpin', icon: "icon.svg" },
   },
   {
     location: [-33.8748755, 151.1987113],
     addHandler: "mouseover",
-    infoboxOption: { title: "Ben's lemons", description: 'Contact Ben (+61 465 876 2347) for some lemons' },
+    infoboxOption: { 
+      htmlContent: infoboxTemplate({ 
+        title: ['Contact Ben', '(+61 465 876 2347)', 'for some lemons'], 
+        description: ['Contact Ben', '(+61 465 876 2347)', 'for some lemons'],
+        image: 'lemons.jpg'
+      }) 
+    },
     pushPinOption: { title: "Ben's lemons", description: 'Pushpin', icon: "icon.svg" },
   },
   {
     location: [-33.8282967, 151.2311086],
     addHandler: "mouseover",
-    infoboxOption: { title: "Zyrah's lemonlicious lemons", description: 'Contact Zyrah (+61 465 876 2348) for some lemons' },
+    infoboxOption: { 
+      htmlContent: infoboxTemplate({ 
+      title: "Zyrah's lemonlicious lemons", 
+      description: ['Contact Zyrah', '(+61 465 876 2348)', 'for some lemons'],
+      image: 'lemons.jpg' 
+    }) 
+  },
     pushPinOption: { title: "Zyrah's lemonlicious lemons", description: 'Pushpin', icon: "icon.svg" },
   }
 ];
@@ -31,31 +68,61 @@ const herbPushPins = [
   {
     location: [-33.8832331, 151.1983055],
     addHandler: "mouseover",
-    infoboxOption: { title: "Fred's fresh mint leaves", description: 'Contact Fred (+61 465 876 2349) for some mint' },
+    infoboxOption: { 
+      htmlContent: infoboxTemplate({ 
+        title: "Fred's fresh mint leaves", 
+        description: ['Contact Fred', '(+61 465 876 2349)', 'for some mint'],
+        image: 'herbs.jpg'
+      }) 
+    },
     pushPinOption: { title: "Fred's fresh mint leaves", description: 'Pushpin', icon: "icon.svg" },
   },
   {
     location: [-33.8571441, 151.1902714],
     addHandler: "mouseover",
-    infoboxOption: { title: "Jane's fresh basil leaves", description: 'Contact Jane (+61 465 876 2349) for some basil' },
+    infoboxOption: { 
+      htmlContent: infoboxTemplate({ 
+        title: "Jane's fresh basil leaves", 
+        description: ['Contact Jane', '(+61 465 876 2349)', 'for some basil'],
+        image: 'herbs.jpg' 
+      }) 
+    },
     pushPinOption: { title: "Jane's fresh basil leaves", description: 'Pushpin', icon: "icon.svg" },
   },
   {
     location: [-33.8658159, 151.2046359],
     addHandler: "mouseover",
-    infoboxOption: { title: "Kelly's fresh rosemary leaves", description: 'Contact Kelly (+61 465 876 2311) for some rosemary' },
+    infoboxOption: { 
+      htmlContent: infoboxTemplate({ 
+        title: "Kelly's fresh rosemary leaves", 
+        description: ['Contact Kelly', '(+61 465 876 2311)', 'for some rosemary'],
+        image: 'herbs.jpg' 
+      }) 
+    },
     pushPinOption: { title: "Kelly's fresh rosemary leaves", description: 'Pushpin', icon: "icon.svg" },
   },
   {
     location: [-33.8658114, 151.2046359],
     addHandler: "mouseover",
-    infoboxOption: { title: "Sam's fresh peppermint leaves", description: 'Contact Sam (+61 465 876 2311) for some peppermint' },
+    infoboxOption: { 
+      htmlContent: infoboxTemplate({ 
+        title: "Sam's fresh peppermint leaves", 
+        description: ['Contact Sam', '(+61 465 876 2311)', 'for some peppermint'], 
+        image: 'herbs.jpg' 
+      }) 
+    },
     pushPinOption: { title: "Sam's fresh peppermint leaves", description: 'Pushpin', icon: "icon.svg" },
   },
   {
     location: [-33.8929838, 151.2458842],
     addHandler: "mouseover",
-    infoboxOption: { title: "Josie's fresh coriander leaves", description: 'Contact Josie (+61 465 876 2317) for some coriander' },
+    infoboxOption: { 
+      htmlContent: infoboxTemplate({ 
+        title: "Josie's fresh coriander leaves", 
+        description: ['Contact Josie', '(+61 465 876 2317)', 'for some coriander'], 
+        image: 'herbs.jpg'  
+      }) 
+    },
     pushPinOption: { title: "Josie's fresh coriander leaves", description: 'Pushpin', icon: "icon.svg" },
   }
 ];
@@ -63,13 +130,24 @@ const flowerPushPins = [
   {
     location: [-33.7957454, 151.1723844],
     addHandler: "mouseover",
-    infoboxOption: { title: "Ruby's bright red roses", description: 'Contact Ruby (+61 465 876 2312) for some red roses' },
+    infoboxOption: { 
+      htmlContent: infoboxTemplate({ 
+        title: "Ruby's bright red roses", 
+        description: ['Contact Ruby', '(+61 465 876 2312)', 'for some red roses'], 
+        image: 'roses.jpg'  
+      }) 
+    },
     pushPinOption: { title: "Ruby's bright red roses", description: 'Pushpin', icon: "icon.svg" },
   },
   {
     location: [-33.8366073, 151.2052079],
     addHandler: "mouseover",
-    infoboxOption: { title: "Danny's sweet sunflowers", description: 'Contact Danny (+61 465 876 2313) for some sunflowers' },
+    infoboxOption: { htmlContent: infoboxTemplate({ 
+      title: "Danny's sweet sunflowers", 
+      description: ['Contact Danny', '(+61 465 876 2313)', 'for some sunflowers'], 
+      image: 'sunflowers.jpg'   
+    }) 
+  },
     pushPinOption: { title: "Danny's sweet sunflowers", description: 'Pushpin', icon: "icon.svg" },
   }
 ];
@@ -77,19 +155,34 @@ const figPushPins = [
   {
     location: [-33.8402663, 151.1944232],
     addHandler: "mouseover",
-    infoboxOption: { title: "Poppy's sweet, juicy figs", description: 'Contact Poppy (+61 465 876 2314) for some sunflowers' },
+    infoboxOption: { htmlContent: infoboxTemplate({ 
+      title: "Poppy's sweet, juicy figs", 
+      description: ['Contact Poppy', '(+61 465 876 2314)', 'for some sunflowers'], 
+      image: 'figs.jpg' 
+    }) 
+  },
     pushPinOption: { title: "Poppy's sweet, juicy figs", description: 'Pushpin', icon: "icon.svg" },
   },
   {
     location: [-33.8471022, 151.2093551],
     addHandler: "mouseover",
-    infoboxOption: { title: "Karen's delicious figs", description: 'Contact Karen (+61 465 876 2315) for some sunflowers' },
+    infoboxOption: { htmlContent: infoboxTemplate({ 
+      title: "Karen's delicious figs", 
+      description: ['Contact Karen', '(+61 465 876 2315)', 'for some sunflowers'], 
+      image: 'figs.jpg'  
+    }) 
+  },
     pushPinOption: { title: "Karen's delicious figs", description: 'Pushpin', icon: "icon.svg" },
   },
   {
     location: [-33.8587523, 151.2058246],
     addHandler: "mouseover",
-    infoboxOption: { title: "Chris' nice figs", description: 'Contact Chris (+61 465 876 2316) for some sunflowers' },
+    infoboxOption: { htmlContent: infoboxTemplate({ 
+      title: "Chris' nice figs", 
+      description: ['Contact Chris', '(+61 465 876 2316)', 'for some sunflowers'], 
+      image: 'figs.jpg' 
+    }) 
+  },
     pushPinOption: { title: "Chris' nice figs", description: 'Pushpin', icon: "icon.svg" },
   }
 ];
